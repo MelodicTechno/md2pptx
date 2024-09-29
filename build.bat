a28c97862dfbdc08502f04bec08eee74633bb5ae
MelodicTechno <617179025@qq.com>
2024-09-29 20:24:40 +0800

    ignore build

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+@REM build
 cmake -S . -B build/ -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release
 cmake --build build/ --config Release
+@REM run exe
 .\build\Release\MarkdownToPPTX.exe
