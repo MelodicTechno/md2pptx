36eabb353cfc120b1c6db02bcc43e8bb395d2b7e
MelodicTechno <617179025@qq.com>
2024-10-07 09:51:08 +0800

    add PageParser

---
+++ TODO.md
@@ -0,0 +1,7 @@
+take an input of markdown files
+
+parse the md into xml
+
+come up with a way to seperate the md into pages
+
+parse the xml to pptx or sth else
---
+++ include/mdtoppt/PageParser.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "maddy/parser.h"
+#include <string>
+#include <regex>
+
+// i want to name the namespace as page but i would cause trouble
+namespace maddy
+{
+    class PageParser : public LineParser
+    {
+    private:
+        int pageNo;
+    public:
+        PageParser() : pageNo(1) {}
+        void Parse(std::string &line) override
+        {
+            // new page with +----+
+            static const std::regex re(R"(^\s*+----+\s*$)", std::regex_constants::ECMAScript);
+            // <page number="pageNo"\>
+            std::string replacement = "<page number=\"" + std::to_string(pageNo) + "\">";
+
+            line = std::regex_replace(line, re, replacement);
+            pageNo++;
+        }
+    };
+}
